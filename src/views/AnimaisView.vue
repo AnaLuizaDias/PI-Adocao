054d60485ce3f151c766eda0e75b8f8ea51cf38f
Ana Luiza <analuizadiasdarocha212@gmail.com>
2022-05-22 12:52:07 -0300

    ok

---
+++ src/views/AnimaisView.vue
@@ -0,0 +1,155 @@
+<template>
+  <div class="margin">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <div class="card">
+          <img
+            id="oi"
+            src="../assets/cat.jpeg"
+            class="card-img-top img-cards"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">Frajola</h5>
+            <p class="card-text">
+              Amo pular, sou muito ciumenta com meus brinquedos
+              e não deixo outros animais pegarem. Estou a procura de donos que 
+              estejam dispostos a dar muito amor!
+            </p>
+            <button type="button" class="btn btn-secondary botao-meu">
+              Ver mais
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../assets/cat2.jpg" class="card-img-top img-cards" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Bola</h5>
+            <p class="card-text">
+              Pelo meu nome já se sabe que eu sou um cão que ama brincar
+              com uma bola... Sou muito dócil e fofo, adoro correr e passear
+              de coleira.
+            </p>
+            <button type="button" class="btn btn-secondary botao-meu">
+              Ver mais
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../assets/dog.jpg" class="card-img-top img-cards" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Amora</h5>
+            <p class="card-text">
+              Meu nome é Amora, e tenho apenas 1 mês de idade, sou muito 
+              dócil e amo brincar, correr, e sou um pouco bagunceira também, 
+              busco amor e carinho...
+            </p>
+            <button type="button" class="btn btn-secondary botao-meu">
+              Ver mais
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../assets/dog1.png" class="card-img-top img-cards" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Lili</h5>
+            <p class="card-text">
+              Sou um pouco brava, mas é pra proteger aqueles que amo,
+              adoro brincar com outros cães, mas não gosto de gatos...
+              Amo passear
+            </p>
+            <button type="button" class="btn btn-secondary botao-meu">
+              Ver mais
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../assets/cat.jpeg" class="card-img-top img-cards" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Felipe</h5>
+            <p class="card-text">
+              Amo pular, sou muito ciumento com meus brinquedos
+              e não deixo outros animais pegarem. Estou a procura de donos que 
+              estejam dispostos a dar muito amor!
+            </p>
+            <button type="button" class="btn btn-secondary botao-meu">
+              Ver mais
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../assets/cat2.jpg" class="card-img-top img-cards" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Luna</h5>
+            <p class="card-text">
+              Olá, sou a gatinha do meio, e sou a única dos meus irmãos
+              que ainda precisa de um lar... Sou muito dorminhoca, e quase
+              não dou trabalho.
+            </p>
+            <button type="button" class="btn btn-secondary botao-meu">
+              Ver mais
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../assets/dog.jpg" class="card-img-top img-cards" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Bola</h5>
+            <p class="card-text">
+              Pelo meu nome já se sabe que eu sou um cão que ama brincar
+              com uma bola... Sou muito dócil e fofo, adoro correr e passear
+              de coleira.
+            </p>
+            <button type="button" class="btn btn-secondary botao-meu">
+              Ver mais
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../assets/dog1.png" class="card-img-top img-cards" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Totó</h5>
+            <p class="card-text">
+              Sou um pouco bravo, mas é pra proteger aqueles que amo,
+              adoro brincar com outros cães, mas não gosto de gatos...
+              Amo passear
+            </p>
+            <button type="button" class="btn btn-secondary botao-meu">
+              Ver mais
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../assets/cat.jpeg" class="card-img-top img-cards" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Jill</h5>
+            <p class="card-text">
+              Amo pular, sou muito ciumenta com meus brinquedos
+              e não deixo outros animais pegarem. Estou a procura de donos que 
+              estejam dispostos a dar muito amor!
+            </p>
+            <button type="button" class="btn btn-secondary botao-meu">
+              Ver mais
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
